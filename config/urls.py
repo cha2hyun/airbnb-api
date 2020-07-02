b96b08e3cdb60f05669631a45b6efbc4b72c30aa
cha2hyun-dev <cha2hyun.dev@gmail.com>
2020-07-02 17:58:24 +0900

    #2.0 ListRoomsView & SeeRoomsView

--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
- 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("rooms/", include("core.urls")),
     path("api/v1/rooms/", include("rooms.urls")),
 ]
 
--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,6 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from .models import Room
-from .serializers import RoomSerializer, BigRoomSerializer
+from .serializers import RoomSerializer
 
 
 class ListRoomsView(ListAPIView):
@@ -9,7 +9,7 @@ class ListRoomsView(ListAPIView):
 
 class SeeRoomView(RetrieveAPIView):
     queryset = Room.objects.all()
-    serializer_class = BigRoomSerializer
+    serializer_class = RoomSerializer
 
 
 # class based view wit APIview
