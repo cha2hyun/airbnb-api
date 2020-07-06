0df4cf431396f3dfcd2913c322659adb12d9f899
cha2hyun-dev <cha2hyun.dev@gmail.com>
2020-07-06 16:06:28 +0900

    #3.5 Users Viewset

--- users/views.py
+++ users/views.py
@@ -6,20 +6,21 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser, AllowAny
 from rooms.serializers import RoomSerializer
 from rooms.models import Room
 from .models import User
 from .serializers import UserSerializer
 
+class UsersViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    def get_permissions(self):
+        if self.action == "list":
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
 
-class UsersView(APIView):
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            new_user = serializer.save()
-            return Response(UserSerializer(new_user).data)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class MeView(APIView):
@@ -86,3 +87,14 @@ def login(request):
         return Response(data={"token": encoded_jwt})
     else:
         return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+
+
+# class UsersView(APIView):
+#     def post(self, request):
+#         serializer = UserSerializer(data=request.data)
+#         if serializer.is_valid():
+#             new_user = serializer.save()
+#             return Response(UserSerializer(new_user).data)
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
